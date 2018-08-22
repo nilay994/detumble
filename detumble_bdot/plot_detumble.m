6d0414679a3fb0f38e30788df0b871db3aae5019
legorative <nilay_994@hotmail.com>
2018-08-22 14:38:03 +0200

    linkaxes - comparing plots is easier

--- detumble_bdot/plot_detumble.m
+++ detumble_bdot/plot_detumble.m
@@ -4,62 +4,68 @@ readMatrix_r = csvread('data4test_02_in.csv', 1, 0);
 readMatrix_n = csvread('data4test_02_out.csv', 1, 0);
 
 plot_save1 = figure(1);
-subplot(2,1,1);
+pu1 = subplot(2,1,1);
 plot(readMatrix_r(:,13)); hold on; plot(readMatrix_r(:,14)); plot(readMatrix_r(:,15)); grid on;
 title('Roberts algorithm');
 xlabel('time/sample');
 ylabel('p_{tumb}');
 
-subplot(2,1,2);
+pu2 = subplot(2,1,2);
 plot(readMatrix_n(:,13)); hold on; plot(readMatrix_n(:,14)); plot(readMatrix_n(:,15)); grid on;
 title('Nilays algorithm');
 xlabel('time/sample');
 ylabel('p_{tumb}');
+linkaxes([pu1,pu2], 'xy');
 saveas(plot_save1,'cmp1','jpg');
 
 %%
 plot_save2 = figure(2);
-subplot(3,2,1);
+py1 = subplot(3,2,1);
 plot(readMatrix_r(:,10)); grid on;
 title('Roberts algorithm');
 xlabel('time/sample');
 ylabel('s_{on}');
-subplot(3,2,3);
+py3 = subplot(3,2,3);
 plot(readMatrix_r(:,11)); grid on;
-subplot(3,2,5);
+py5 = subplot(3,2,5);
 plot(readMatrix_r(:,12)); grid on;
 
-subplot(3,2,2);
+py2 = subplot(3,2,2);
 plot(readMatrix_n(:,10)); grid on;
 title('Nilays algorithm');
 xlabel('time/sample');
 ylabel('s_{on}');
-subplot(3,2,4);
+py4 = subplot(3,2,4);
 plot(readMatrix_n(:,11)); grid on;
-subplot(3,2,6);
+py6 = subplot(3,2,6);
 plot(readMatrix_n(:,12)); grid on;
 saveas(plot_save2,'cmp2','jpg');
-
+linkaxes([py1,py2,py3,py4,py5,py6],'xy');
 %% 
 plot_save3 = figure(3);
-subplot(3,2,1);
+
+pt1 = subplot(3,2,1);
 plot(readMatrix_r(:,7)); grid on;
 title('Roberts algorithm');
 xlabel('time/sample');
 ylabel('t_{on}');
-subplot(3,2,3);
+
+pt3 = subplot(3,2,3);
 plot(readMatrix_r(:,8)); grid on;
-subplot(3,2,5);
+
+pt5 = subplot(3,2,5);
 plot(readMatrix_r(:,9)); grid on;
 
-subplot(3,2,2);
+pt2 = subplot(3,2,2);
 plot(readMatrix_n(:,7)); grid on;
 title('Nilays algorithm');
 xlabel('time/sample');
 ylabel('t_{on}');
-subplot(3,2,4);
+
+pt4 = subplot(3,2,4);
 plot(readMatrix_n(:,8)); grid on;
-subplot(3,2,6);
+pt6 = subplot(3,2,6);
 plot(readMatrix_n(:,9)); grid on;
+linkaxes([pt1,pt2,pt3,pt4,pt5,pt6],'xy');
 
 saveas(plot_save3,'cmp3','jpg');
